033ea776dc70055663f8d337f1a6f4ebd82be036
Switched <y.asare@ymail.com>
2011-10-30 00:06:34 -0500

    $ git commit -m first upload of rails files

---
+++ tumor/config/initializers/cookie_verification_secret.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.cookie_verifier_secret = '9678be548cef5af66e1d604b8e7734def2160be2e891d62f2e10309b9ce4c3c9bcd2e63d53bb5804554f54f0db159466bde57353588f202821f0354bd71f52ca';
